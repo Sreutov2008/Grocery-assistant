b6934781704160785df1c220329f2374f8d012eb
Александр Реутов <sreutov2008@yandex.ru>
2023-05-12 23:53:04 +0700

    Замечания подготоµплоя на сервер

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,9 +8,6 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', default='u*-#rt#6=r_a1j6m_i0af=)ro1r3c1*476pl#*h_)00q-dt!$t')
 
