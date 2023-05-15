3a1033db5347446dda13cb2b549c8c007c4d047b
Александр Реутов <sreutov2008@yandex.ru>
2023-05-15 23:45:30 +0700

    deploy

--- backend/api/views.py
+++ backend/api/views.py
@@ -101,6 +101,7 @@ class RecipeViewSet(ModelViewSet):
     )
     def download_shopping_cart(self, request):
         """Метод для скачивания списка покупок."""
+        user = request.user
         if not user.shoppingcarts.exists():
             return Response(status=HTTP_400_BAD_REQUEST)
         ingredients = IngredientInRecipe.objects.filter(
